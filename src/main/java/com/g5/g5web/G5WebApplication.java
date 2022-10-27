56b1614c760e71d2090ad46a83cc4e0d868843c4
Leonardo Lopez Peve <leonardolopezpeve@hotmail.com>
2022-10-27 17:07:51 -0500

    Proyecto inicial con configuraciones iniciales

---
+++ src/main/java/com/g5/g5web/G5WebApplication.java
@@ -0,0 +1,13 @@
+package com.g5.g5web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G5WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G5WebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/g5/g5web/G5WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g5.g5web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G5WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
